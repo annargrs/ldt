d99dc6cefdafdb8605019696f0f1b26abc809a06
ookimi <anna.gld@gmail.com>
2018-08-12 19:34:14 -0400

    main inits

--- ldt/__init__.py
+++ ldt/__init__.py
@@ -9,25 +9,51 @@ Subpackages
 
 """
 
-from ._version import __version__
+# from . import _version
 # from .load_config import config
 
-import ldt.helpers
-import ldt.dicts
+from ldt.helpers import resources
+from ldt.helpers import loading
+from ldt.helpers import ignore
+from ldt.helpers import wiktionary_cache
+from ldt.helpers import exceptions
+from ldt.helpers import formatting
+
+from ldt.dicts import dictionary
+from ldt.dicts import metadictionary
+from ldt.dicts import resources
+
+from ldt.dicts.base.custom import en
+from ldt.dicts.base.wordnet import en
+from ldt.dicts.base import babelnet
+from ldt.dicts.base import wiktionary
+
+from ldt.dicts.morphology.custom import en
+from ldt.dicts.morphology.wordnet import en
+from ldt.dicts.morphology import babelnet
+from ldt.dicts.morphology import wiktionary
+from ldt.dicts.morphology import morph_dictionary
+
+from ldt.dicts.spellcheck.en import en
+from ldt.dicts.spellcheck import custom
+
+from ldt.dicts.semantics.wordnet import en
+from ldt.dicts.semantics import babelnet
+from ldt.dicts.semantics import wiktionary
+from ldt.dicts.semantics import wikisaurus
+from ldt.dicts.semantics import lex_dictionary
+
+from ldt.dicts.derivation.custom import en
+from ldt.dicts.derivation.wordnet import en
+from ldt.dicts.derivation import wiktionary
+
+
+
+# import ldt.helpers
+# import ldt.dicts
 # import ldt.dicts.semantics
 # import ldt.dicts.spellcheck
 # import ldt.dicts.morphology
 # import ldt.dicts.derivation
 # import ldt.dicts.base
-
-# from .helpers import loading, formatting, wiktionary_cache, resources
-#
-# from .dicts.dictionary import *
-# from .dicts.metadictionary import *
-# from .dicts.base import *
-# from .dicts.semantics import *
-# from .dicts.morphology import *
-# from .dicts.derivation import *
-# from .dicts.spellcheck import *
-# from .dicts.resources import *
 
