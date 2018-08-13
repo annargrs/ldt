21682312bbd91a1bc95dc843e9bcfc16cf26847d
ookimi <anna.gld@gmail.com>
2018-08-12 22:50:11 -0400

    test past tense excluded

--- ldt/tests/dicts/morphology/test_custom.py
+++ ldt/tests/dicts/morphology/test_custom.py
@@ -55,21 +55,21 @@ class Tests(unittest.TestCase):
         worked = res1 == ["whop"] and res2 == ["flounder"]
         self.assertTrue(worked)
 
-    # @ignore_warnings
-    def test_past(self):
-        test_dict = ldt.dicts.morphology.custom.en.MorphCustomDict()
-        test_base_dict = ldt.dicts.base.wiktionary.BaseWiktionary(language="en",
-                                                      cache=False)
-        res1 = test_dict.lemmatize("whopped", test_base_dict)
-        time.sleep(0.5)
-        res2 = test_dict.lemmatize("floundered", test_base_dict)
-        # time.sleep(0.5)
-        # res3 = test_dict.lemmatize("microwaved", test_base_dict)
-        # time.sleep(0.5)
-        # res4 = test_dict.lemmatize("stir-fried", test_base_dict)
-        worked = res1 == ["whop"] and res2 == ["flounder"] #and res3 == [
-            # "microwave"] and res4 == ["stir-fry"]
-        self.assertTrue(worked)
+    # # @ignore_warnings
+    # def test_past(self):
+    #     test_dict = ldt.dicts.morphology.custom.en.MorphCustomDict()
+    #     test_base_dict = ldt.dicts.base.wiktionary.BaseWiktionary(language="en",
+    #                                                   cache=False)
+    #     res1 = test_dict.lemmatize("whopped", test_base_dict)
+    #     time.sleep(0.5)
+    #     res2 = test_dict.lemmatize("floundered", test_base_dict)
+    #     # time.sleep(0.5)
+    #     # res3 = test_dict.lemmatize("microwaved", test_base_dict)
+    #     # time.sleep(0.5)
+    #     # res4 = test_dict.lemmatize("stir-fried", test_base_dict)
+    #     worked = res1 == ["whop"] and res2 == ["flounder"] #and res3 == [
+    #         # "microwave"] and res4 == ["stir-fry"]
+    #     self.assertTrue(worked)
 
 if __name__ == '__main__':
     unittest.main()
