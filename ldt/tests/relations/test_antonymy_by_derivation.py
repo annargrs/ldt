89378716653ea6329b48428213d4a59c9c78f1e0
ookimi <anna.gld@gmail.com>
2018-09-11 22:57:22 -0400

    out of memory error fix

--- ldt/tests/relations/test_antonymy_by_derivation.py
+++ ldt/tests/relations/test_antonymy_by_derivation.py
@@ -7,67 +7,76 @@ import ldt
 
 from ldt.helpers.ignore import ignore_warnings as ignore_warnings
 
-# cat = ldt.relations.word.Word("cat")
-irregular = ldt.relations.word.Word("irregular")
-regular = ldt.relations.word.Word("regular")
-careful = ldt.relations.word.Word("careful")
-careless = ldt.relations.word.Word("careless")
-care = ldt.relations.word.Word("care")
-cat = ldt.relations.word.Word("cat")
-prewar = ldt.relations.word.Word("pre-war")
-postwar = ldt.relations.word.Word("post-war")
-
-
-test_dict = ldt.relations.antonymy_by_derivation.DerivationalAntonymy(
-    language="English")
-
 class Tests(unittest.TestCase):
     """
     The tests in this block inspect detection of antonymy via producuctive
     derivational patterns.
     """
 
+    @classmethod
+    @ignore_warnings
+    def setUpClass(cls):
+        cls.cat = ldt.relations.word.Word("cat")
+        cls.irregular = ldt.relations.word.Word("irregular")
+        cls.regular = ldt.relations.word.Word("regular")
+        cls.careful = ldt.relations.word.Word("careful")
+        cls.careless = ldt.relations.word.Word("careless")
+        cls.care = ldt.relations.word.Word("care")
+        cls.prewar = ldt.relations.word.Word("pre-war")
+        cls.postwar = ldt.relations.word.Word("post-war")
+        cls.test_dict = ldt.relations.antonymy_by_derivation.DerivationalAntonymy(
+            language="English")
+
     def test_init(self):
         """Test initialization."""
-        self.assertEqual(test_dict.language, "en")
+        self.assertEqual(self.test_dict.language, "en")
 
     def test_resources(self):
         """Loading of resources."""
-        self.assertIn("ir-", test_dict.resources["Prefixes"])
+        self.assertIn("ir-", self.test_dict.resources["Prefixes"])
 
     def test_antonymy(self):
         """Detecting antonymy in prefixes."""
-        self.assertTrue(test_dict.detect_antonymy(regular, irregular))
+        self.assertTrue(self.test_dict.detect_antonymy(self.regular,
+                                                       self.irregular))
 
     def test_antonymy_reverse(self):
         """Detecting antonymy in prefixes."""
-        self.assertTrue(test_dict.detect_antonymy(irregular, regular))
+        self.assertTrue(self.test_dict.detect_antonymy(self.irregular,
+                                                   self.regular))
 
     def test_antonymy_sanity(self):
         """Sanity check for antonymy in prefixes."""
-        self.assertFalse(test_dict.detect_antonymy(irregular, cat))
+        self.assertFalse(self.test_dict.detect_antonymy(self.irregular,
+                                                       self.cat))
 
     def test_antonymy_suf(self):
         """Detecting antonymy in suffixes."""
-        self.assertTrue(test_dict.detect_antonymy(care, careless))
+        self.assertTrue(self.test_dict.detect_antonymy(self.care,
+                                                       self.careless))
 
     def test_antonymy_suf_reverse(self):
         """Detecting antonymy in suffixes in reverse."""
-        self.assertTrue(test_dict.detect_antonymy(careless, care))
+        self.assertTrue(self.test_dict.detect_antonymy(self.careless,
+                                                       self.care))
 
     def test_antonymy_suf_pair(self):
         """Detecting antonymy in suffix pairs."""
-        self.assertTrue(test_dict.detect_antonymy(careful, careless))
+        self.assertTrue(self.test_dict.detect_antonymy(self.careful,
+                                                    self.careless))
 
     def test_antonymy_suf_pair_reverse(self):
         """Detecting antonymy in suffix pairs in reverse."""
-        self.assertTrue(test_dict.detect_antonymy(careless, careful))
+        self.assertTrue(self.test_dict.detect_antonymy(self.careless,
+                                                    self.careful))
 
     def test_antonymy_pref_pair(self):
         """Detecting antonymy in prefix pairs."""
-        self.assertTrue(test_dict.detect_antonymy(prewar, postwar))
+        self.assertTrue(self.test_dict.detect_antonymy(self.prewar,
+                                                      self.postwar))
 
     def test_antonymy_pref_pair_reverse(self):
         """Detecting antonymy in prefix pairs in reverse."""
-        self.assertTrue(test_dict.detect_antonymy(postwar, prewar))
+        self.assertTrue(self.test_dict.detect_antonymy(self.postwar,
+                                                      self.prewar))
 
