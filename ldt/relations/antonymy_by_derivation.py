5eaaa66c75ecb83f618be43397b3b159c63f2835
ookimi <anna.gld@gmail.com>
2018-09-11 20:38:02 -0400

    analysis of derivational antonymy relations

--- ldt/relations/antonymy_by_derivation.py
+++ ldt/relations/antonymy_by_derivation.py
@@ -2,14 +2,19 @@
 """Sometimes dictionaries lack antonymy relations for derivational pairs
 where one of the word has a negative suffix or prefix (e.g. *regular ~
 irregular*). LDT attempts to establish the relation through lists of
-such language-specific suffixes/prefixes.
+such language-specific suffixes/prefixes that negate the meaning of the stem.
+
+It is also possible to detect pairs of words with complementary affixes,
+such as *-ful* and *-less* in *careful : careless*.
 
 Example:
-    >>> test_dict = ldt.relations.RelationsInPair()
-    >>> test_dict.analyze("black", "white")
-    ['SharedPOS', 'SharedMorphForm', 'Antonyms']
-    >>> test_dict.analyze("happy", "happily")
-    ['Synonyms', 'SharedMorphForm', 'SharedDerivation']
+    >>> test_dict = ldt.relations.antonymy_by_derivation.DerivationalAntonymy(language="English")
+    >>> test_dict.detect_anonymy("regular", "irregular")
+    True
+    >>> test_dict.detect_anonymy("pre-war", "post-war")
+    True
+    >>> test_dict.detect_anonymy("regular", "cat")
+    False
 
 """
 
@@ -19,7 +24,8 @@ from ldt.helpers.resources import lookup_language_by_code as \
     lookup_language_by_code
 
 class DerivationalAntonymy(object):
-
+    """Dictionary of language-specific derivational patterns that could be
+    used to detect derivational antonymy."""
     def __init__(self, language):
 
         if len(language) > 2:
@@ -31,33 +37,68 @@ class DerivationalAntonymy(object):
                                       "antonymy_by_derivation/" + language +
                                       ".yaml")
         resources = load_language_file(resources_path, self.language)
-        self.prefixes = resources["prefixes"]
-        self.suffixes = resources["suffixes"]
+        self.resources = resources
 
 
-def detect_antonymy(self, target, neighbor):
-    """
-    establish the antonymy relation
-    :param neighbor, : ldt word object
-    :return: ldt word object
-    """
+    def detect_antonymy(self, target, neighbor):
+        """
+        The main method for detecting derivational antonyms.
+
+        Args:
+
+            target, neighbor (ldt Word obj): the words to check for this
+            relationship;
+
+        Returns:
+             (bool): True if derivational antonymy was detected.
+
+        """
+
+        def detect_affixes(word1, word2, affix="Prefixes"):
+            """Helper function for handling negative prefixes and affixes."""
+            if word1.info[affix]:
+                for i in word1.info[affix]:
+                    if i in self.resources[affix]:
+                        if affix == "Prefixes":
+                            if word2.info["OriginalForm"] in word1.info["Stems"] \
+                                    and i.strip("-") + word2.info["OriginalForm"]\
+                                    == word1.info["OriginalForm"]:
+                                return True
+                        elif affix == "Suffixes":
+                            if word2.info["OriginalForm"] in word1.info["Stems"] \
+                                    and word2.info["OriginalForm"] + i.strip("-") \
+                                    == word1.info["OriginalForm"]:
+                                return True
+            return False
 
 
-    for i in prefixes:
-        if i in neighbor.affixes:
-            for w in list(neighbor.spellings.keys()) + [
-                neighbor.original_spelling]:
-                if w.startswith(i[:-1]):
-                    stem = w[len(i) - 1:]
-                    #                elif w.startswith(i[:-1]):
-                    if stem in target.spellings:
-                        return True
-    for i in suffixes:
-        if i in neighbor.affixes:
-            for w in list(neighbor.spellings.keys()) + [
-                neighbor.original_spelling]:
-                if w.endswith(i[1:]):
-                    stem = w[:len(i)]
-                    if stem in target.spellings:
-                        return True
-    return False
+        def detect_affix_pairs(word1, word2, affix="Prefixes"):
+            """Helper function for handling pairs of affixes. Word length
+            serves as the final check on whether the decomposition is
+            complete."""
+            for pair in self.resources[affix[:-2]+"_pairs"]:
+                if pair[0] in word1.info[affix] and pair[1] in word2.info[affix]:
+                    shared_stem = set(word1.info["Stems"]).intersection(
+                        word2.info["Stems"])
+                    if shared_stem:
+                        len_word = len(list(shared_stem)[0]) + len(pair[0])-1
+                        if len_word == len(word1.info["OriginalForm"]):
+                            return True
+                        elif word1.info["OriginalForm"].count("-") == 1 and \
+                                len_word+1 == len(word1.info["OriginalForm"]):
+                            return True
+            return False
+
+        checks = [detect_affixes(target, neighbor, affix="Prefixes"),
+                  detect_affixes(neighbor, target, affix="Prefixes"),
+                  detect_affixes(neighbor, target, affix="Suffixes"),
+                  detect_affixes(target, neighbor, affix="Suffixes"),
+                  detect_affix_pairs(target, neighbor, affix="Suffixes"),
+                  detect_affix_pairs(neighbor, target, affix="Suffixes"),
+                  detect_affix_pairs(neighbor, target, affix="Prefixes"),
+                  detect_affix_pairs(target, neighbor, affix="Prefixes")]
+
+        for check in checks:
+            if check:
+                return True
+        return False
--- ldt/tests/relations/test_antonymy_by_derivation.py
+++ ldt/tests/relations/test_antonymy_by_derivation.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Testing the analysis of antonymy in by productive derivational
+"""Testing the analysis of antonymy with productive derivational
 patterns."""
 
 import unittest
@@ -10,6 +10,13 @@ from ldt.helpers.ignore import ignore_warnings as ignore_warnings
 # cat = ldt.relations.word.Word("cat")
 irregular = ldt.relations.word.Word("irregular")
 regular = ldt.relations.word.Word("regular")
+careful = ldt.relations.word.Word("careful")
+careless = ldt.relations.word.Word("careless")
+care = ldt.relations.word.Word("care")
+cat = ldt.relations.word.Word("cat")
+prewar = ldt.relations.word.Word("pre-war")
+postwar = ldt.relations.word.Word("post-war")
+
 
 test_dict = ldt.relations.antonymy_by_derivation.DerivationalAntonymy(
     language="English")
@@ -25,6 +32,42 @@ class Tests(unittest.TestCase):
         self.assertEqual(test_dict.language, "en")
 
     def test_resources(self):
-        """Test initialization."""
-        self.assertIn("ir-", test_dict.prefixes)
+        """Loading of resources."""
+        self.assertIn("ir-", test_dict.resources["Prefixes"])
+
+    def test_antonymy(self):
+        """Detecting antonymy in prefixes."""
+        self.assertTrue(test_dict.detect_antonymy(regular, irregular))
+
+    def test_antonymy_reverse(self):
+        """Detecting antonymy in prefixes."""
+        self.assertTrue(test_dict.detect_antonymy(irregular, regular))
+
+    def test_antonymy_sanity(self):
+        """Sanity check for antonymy in prefixes."""
+        self.assertFalse(test_dict.detect_antonymy(irregular, cat))
+
+    def test_antonymy_suf(self):
+        """Detecting antonymy in suffixes."""
+        self.assertTrue(test_dict.detect_antonymy(care, careless))
+
+    def test_antonymy_suf_reverse(self):
+        """Detecting antonymy in suffixes in reverse."""
+        self.assertTrue(test_dict.detect_antonymy(careless, care))
+
+    def test_antonymy_suf_pair(self):
+        """Detecting antonymy in suffix pairs."""
+        self.assertTrue(test_dict.detect_antonymy(careful, careless))
+
+    def test_antonymy_suf_pair_reverse(self):
+        """Detecting antonymy in suffix pairs in reverse."""
+        self.assertTrue(test_dict.detect_antonymy(careless, careful))
+
+    def test_antonymy_pref_pair(self):
+        """Detecting antonymy in prefix pairs."""
+        self.assertTrue(test_dict.detect_antonymy(prewar, postwar))
+
+    def test_antonymy_pref_pair_reverse(self):
+        """Detecting antonymy in prefix pairs in reverse."""
+        self.assertTrue(test_dict.detect_antonymy(postwar, prewar))
 
