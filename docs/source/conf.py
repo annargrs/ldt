db8012fcf32e83d157d854b2887cdf8536c0e7d3
annargrs <gladkova@phiz.c.u-tokyo.ac.jp>
2018-10-08 23:30:19 -0400

    try finding a matplotlib backend that works

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ sys.path.insert(0, root)
 sample_file = os.path.join(root, "ldt/tests/sample_files/.ldt-config.yaml")
 shutil.copyfile(sample_file, tutorial_path)
 
-autodoc_mock_imports = ["nltk", "enchant", "pyenchant", '_tkinter']
+autodoc_mock_imports = ["_tkinter", "nltk", "enchant", "pyenchant"]
 
 # -- General configuration ------------------------------------------------
 
@@ -233,6 +233,18 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-import matplotlib
-matplotlib.use('agg')
+# import matplotlib
+# matplotlib.use('agg')
 
+import matplotlib
+gui_env = [i for i in matplotlib.rcsetup.interactive_bk]
+print ("I will test for", gui_env)
+for gui in gui_env:
+    print ("testing", gui)
+    try:
+        matplotlib.use(gui,warn=False, force=True)
+        from matplotlib import pyplot as plt
+        print ("    ",gui, "Is Available")
+        print ("Using ..... ",matplotlib.get_backend())
+    except:
+        print ("    ",gui, "Not found")
